9b09217ed5ec71fb5de016f24b030bab8fcbdcac
elhameg <azizielhamg@johndeere.com>
2020-12-09 13:38:43 -0600

    auth and users CRUD are completed in postmon

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,4 +9,42 @@ router.get("/profile/:id", async (req, res) => {
   res.json({ user });
 });
 
+// GET ALL USERS
+router.get("/", async (req, res) => {
+  let users = await UserModel.findAll();
+  res.json({
+      users
+  })
+});
+
+// UPDATE A USER
+router.put("/:id", async (req, res) => {
+  let user = await UserModel.update(
+      req.body, {
+          where: {
+              id: req.params.id
+          },
+          returning: true
+      }
+  );
+  res.json({
+      user
+  });
+})
+
+// DELETE A USER
+router.delete("/:id", async (req, res) => {
+  await UserModel.destroy({
+          where: {
+              id: req.params.id
+          }
+      }
+  );
+  res.json({
+      message: `User with id ${req.params.id} was deleted.`
+  })
+
+});
+
+
 module.exports = router;
